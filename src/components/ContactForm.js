8ad6008c59334ec4c2b644843e393940b3a5eb95
IsacHolstad <wisac22@gmail.com>
2023-04-04 15:27:37 +0200

    searchbar is filtering api data by prouduct title

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -63,7 +63,7 @@ const ContactForm = () => {
                 </div>
                 <div className="mt-10 flex justify-end border-t border-gray-900/10 pt-8">
                     <button type="submit"
-                            className="rounded-md bg-green-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Send
+                            className="rounded-md bg-gray-800 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Send
                         message
                     </button>
                 </div>
--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useEffect, useMemo } from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {fetchProducts} from "../../store/modules/productSlice";
-import SearchBar from "../shared/SearchBar";
 function Home() {
     const dispatch = useDispatch();
     const {products} = useSelector(state => state.products);
+    const [search, setSearch] = useState();
+    console.log(search);
     useEffect(() => {
         dispatch(fetchProducts());
     }, [dispatch])
@@ -20,11 +21,20 @@ function Home() {
                     <div className="bg-white">
                         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                             <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products</h2>
-                            <SearchBar/>
-
+                            <input
+                                id="search"
+                                name="search"
+                                onChange={(e) => setSearch(e.target.value)}
+                                className="block w-1/2 rounded-md border-2 border-gray-800 bg-transparent py-1.5 pl-10 pr-3 text-black placeholder:text-gray-400 focus:bg-white focus:text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
+                                placeholder="Search"
+                                type="search"
+                            />
                             <div
                                     className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                                    {memoProducts.map((product) => (
+                                    {memoProducts.filter((product => {
+                                        return search === '' ? product : product.title.
+                                            toLowerCase().includes(search)
+                                    })).map((product) => (
                                         <div key={product.id} className="group relative flex flex-col gap-y-3">
                                             <Link to={`product/${product.id}`}
                                                   className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
