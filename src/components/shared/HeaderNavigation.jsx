7dd9b4beb8565e3003d8df9c6a0f33ad84958b95
IsacHolstad <wisac22@gmail.com>
2023-04-04 12:47:15 +0200

    searchbar moved to only home page

--- src/components/shared/HeaderNavigation.jsx
+++ src/components/shared/HeaderNavigation.jsx
@@ -35,17 +35,6 @@ const HeaderNavigation = () => {
                                 </div>
                             </div>
                             <div className="flex flex-1 justify-center px-2 lg:ml-6 lg:justify-end">
-                                <div className="w-full max-w-lg lg:max-w-xs">
-                                    <label htmlFor="search" className="sr-only">
-                                        Search
-                                    </label>
-                                    <div className="relative">
-                                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                            <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                        </div>
-                                        <SearchBar/>
-                                    </div>
-                                </div>
                             </div>
                             <span className="mr-4 cursor-pointer ">
                                 <div className="ml-4 flow-root lg:ml-6">
--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -3,6 +3,7 @@ import {useEffect, useMemo } from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {fetchProducts} from "../../store/modules/productSlice";
+import SearchBar from "../shared/SearchBar";
 function Home() {
     const dispatch = useDispatch();
     const {products} = useSelector(state => state.products);
@@ -19,7 +20,9 @@ function Home() {
                     <div className="bg-white">
                         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                             <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products</h2>
-                                <div
+                            <SearchBar/>
+
+                            <div
                                     className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                                     {memoProducts.map((product) => (
                                         <div key={product.id} className="group relative flex flex-col gap-y-3">
